5ebf65419349cbc36ec54e4d65a6dfca91b7adb4
tigerwang3133 <63317883+tigerwang3133@users.noreply.github.com>
2022-02-20 16:57:24 -0500

    增加手机移动开发支持，本地服务器地址设置

--- flask_pear/app.py
+++ flask_pear/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+server ="192.168.1.126"
 
 @app.route('/')
 def student():
-   return render_template('index.html')
+   return render_template('index.html',ip="/result")
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
       impath = request.form['impath']
+      
       return render_template("result.html",result = './static/img/test3.jpg',message = 'dry pot bag veg')
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug = True,host=server,port=8000)
