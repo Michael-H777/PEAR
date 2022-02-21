81e480013c05220ddc55ab8c479fb1e23aa8e397
tigerwang3133 <63317883+tigerwang3133@users.noreply.github.com>
2022-02-20 22:41:00 -0500

    added loading when upload

--- flask_pear/app.py
+++ flask_pear/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import base64
+from os.path import exists
+import time
 app = Flask(__name__)
 server ="192.168.1.126"
 
@@ -7,15 +9,24 @@ server ="192.168.1.126"
 def upload():
    if request.method=="POST":
       imgdata=request.form["myimg"]
+      imgfile=request.form["imgfile"]
       imgdata=base64.b64decode(imgdata)
-      with open("photo.png","wb") as f:
+      print(imgfile)
+      with open("./static/images/"+imgfile+".png","wb") as f:
          f.write(imgdata)
    return render_template('uploadpic.html')
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
-   
-   return render_template("result.html")
-
+   if request.method=="POST":
+      imgfile=request.form["imgfile"]
+      path_to_file="static/images/"+imgfile+".png"
+      for i in range(10):
+         if exists(path_to_file):
+            return render_template("result.html",result=path_to_file,message="idk")
+         else:
+            time.sleep(1)
+   return render_template("result.html",result="static/img/test3.png",message="dry pot bag veg")  
+      
 if __name__ == '__main__':
    app.run(debug = True,host=server,port=8000)
