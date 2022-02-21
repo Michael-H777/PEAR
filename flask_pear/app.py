40d29f91799acbffd72bd347c94ce78c1abc55e8
tigerwang3133 <63317883+tigerwang3133@users.noreply.github.com>
2022-02-20 21:18:57 -0500

    add take photo upload

--- flask_pear/app.py
+++ flask_pear/app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import base64
 app = Flask(__name__)
 server ="192.168.1.126"
 
-@app.route('/')
-def student():
-   return render_template('index.html',ip="/result")
+@app.route('/',methods = ['POST', 'GET'])
+def upload():
+   if request.method=="POST":
+      imgdata=request.form["myimg"]
+      imgdata=base64.b64decode(imgdata)
+      with open("photo.png","wb") as f:
+         f.write(imgdata)
+   return render_template('uploadpic.html')
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
-   if request.method == 'POST':
-      impath = request.form['impath']
-      
-      return render_template("result.html",result = './static/img/test3.jpg',message = 'dry pot bag veg')
+   
+   return render_template("result.html")
 
 if __name__ == '__main__':
    app.run(debug = True,host=server,port=8000)
